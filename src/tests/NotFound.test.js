33a86635b5ef7fff90d3bb116c8b51e9751d5d6b
Windson Macedo <windsondev@hotmail.com>
2022-08-16 16:39:36 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './App.test';
+import { NotFound } from '../pages';
+
+describe('Testes do componente <NotFound.js />', () => {
+  test('A pagina deve conter um texto h2, com uma imagem', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i });
+
+    const notFoundImg = screen.getByAltText(/Pikachu crying because the page request/i);
+
+    expect(notFoundTitle).toBeInTheDocument();
+    expect(notFoundImg.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
